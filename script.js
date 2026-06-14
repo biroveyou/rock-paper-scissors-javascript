46f08363aa7ba30e37d13754a3071a7c67e90eea
biroveyou <biroveyou@gmail.com>
2026-06-14 10:58:57 +0000

    Add computerChoice function for randomness

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 "use strict";
 
+function computerChoice() {
+    // Get a random number in a variable
+    const randomNumber = Math.floor(Math.random() * 100);
+    
+    // Decide based on the random number what are the return values
+    if (randomNumber < 33) {
+        return "rock";
+    } else if (randomNumber > 66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
